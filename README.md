88ed444d0f51714c984400f45560d9c91aac72f9
Zayan <zayan.qazi.mail@gmail.com>
2025-03-16 16:31:32 -0500

    added aliases

---
+++ README.md
@@ -0,0 +1,5 @@
+# Dotfiles
+
+This repository contains my personal dotfiles for customizing my Linux environment.
+
+Note: `example.aliases.sh` was directly copied from `.oh-my-bash/custom/aliases`.
---
+++ example.aliases.sh
@@ -0,0 +1,6 @@
+# Add your own custom alias in the custom/aliases directory. Aliases placed
+# here will override ones with the same name in the main alias directory.
+
+alias activatevirtualenv='source .venv/bin/activate'
+alias zprepos='cd ~/Documents/code/personal_repos/'
+alias zorepos='cd ~/Documents/code/others_repos/'
