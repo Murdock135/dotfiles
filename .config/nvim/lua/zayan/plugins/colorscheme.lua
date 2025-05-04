2f65c1dfc97e0f56dc08db1b5d01402841606de9
Zayan <zayan.qazi.mail@gmail.com>
2025-05-04 11:14:44 -0500

    new nvim colorscheme

--- .config/nvim/lua/zayan/plugins/colorscheme.lua
+++ .config/nvim/lua/zayan/plugins/colorscheme.lua
@@ -1,13 +1,22 @@
 return {
-  {
-    "ntk148v/habamax.nvim",
-    dependencies = { "rktjmp/lush.nvim" },
-    config = function()
-      vim.cmd("colorscheme habamax")
-      vim.o.background = "dark" -- Or "light" if you prefer
-    end,
-  }
+  "forest-nvim/sequoia.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.cmd("colorscheme sequoia") -- or 'sequoia-night' / 'sequoia-rise'
+  end
 }
+
+-- return {
+--   {
+--     "ntk148v/habamax.nvim",
+--     dependencies = { "rktjmp/lush.nvim" },
+--     config = function()
+--       vim.cmd("colorscheme habamax")
+--       vim.o.background = "dark" -- Or "light" if you prefer
+--     end,
+--   }
+-- }
 -- return {
 --  { "ficcdaf/ashen.nvim" },
 --   {
