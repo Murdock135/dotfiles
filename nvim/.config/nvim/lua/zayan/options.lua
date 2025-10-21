82b0520b4d0f84abffd1d4a2e6963d6bcb4ef1fd
Zayan <zayan.qazi.mail@gmail.com>
2025-10-21 12:44:46 -0400

    added gitsigns, nvim-cmp and indent-blankline to nvim config

--- nvim/.config/nvim/lua/zayan/options.lua
+++ nvim/.config/nvim/lua/zayan/options.lua
@@ -2,3 +2,7 @@ vim.opt.relativenumber = true
 vim.opt.wrap = true
 vim.opt.winborder = 'rounded'
 vim.opt.clipboard = 'unnamedplus'
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
---
+++ nvim/.config/nvim/lua/zayan/plugins/cmp.lua
@@ -0,0 +1,29 @@
+return {
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+        'L3MON4D3/LuaSnip',
+        'hrsh7th/cmp-buffer',
+        'hrsh7th/cmp-nvim-lsp',
+        'saadparwaiz1/cmp_luasnip',
+    },
+    config = function()
+        local cmp = require('cmp')
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    require('luasnip').lsp_expand(args.body)
+                end,
+            },
+            mapping = cmp.mapping.preset.insert({
+                ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                ['<Tab>'] = cmp.mapping.select_next_item(),
+                ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+            }),
+            sources = {
+                { name = 'nvim_lsp' },
+                { name = 'luasnip' },
+                { name = 'buffer' },
+            },
+        })
+    end,
+}
